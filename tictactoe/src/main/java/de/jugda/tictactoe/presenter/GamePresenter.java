5965a94684ad05139e71160bab0bdc1d12d78a33
Mopelchen <momo@newgods.org>
2014-02-15 17:42:40 +0100

    Sorry for the mess. :P

--- tictactoe/src/main/java/de/jugda/tictactoe/presenter/GamePresenter.java
+++ tictactoe/src/main/java/de/jugda/tictactoe/presenter/GamePresenter.java
@@ -1,18 +1,29 @@
 package de.jugda.tictactoe.presenter;
 
+import de.jugda.tictactoe.view.BoardView;
 import tictactoe.core.Coordinate;
 import tictactoe.core.Value;
 
 public class GamePresenter {
 
+	private BoardView view;	
+	private Game game;
+	
   public void setField(Coordinate c)
   {
 	  // Value probably depends to player in model
+	  
+	  // Check with game engine, if move is okay
+	  
+	  // if okay, change state of field in model
+	  // and update the view
+	  
   }
   
   public void resetBoard()
   {
-	  
+	  game = new Game(1);
+	  view.resetAllButtons();
   }
   
 }
